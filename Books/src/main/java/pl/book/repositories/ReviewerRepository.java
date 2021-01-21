81fbb51550fdbed48d0cbcbf23a3db95cb0835a4
Wojciech Jurek <wojciech.jurek7@gmail.com>
2021-01-21 19:15:54 +0100

    Dodanie metod szukających oraz widoków dla Marks oraz Reviewers.

--- Books/src/main/java/pl/book/repositories/ReviewerRepository.java
+++ Books/src/main/java/pl/book/repositories/ReviewerRepository.java
@@ -1,9 +1,12 @@
 package pl.book.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import pl.book.entities.Mark;
 import pl.book.entities.Reviewer;
 
 public interface ReviewerRepository extends CrudRepository<Reviewer, Long> {
-
+	@Query("SELECT r FROM Reviewer r WHERE r.reviewer_id = ?1")
+	Iterable<Reviewer> findAllWhereId(Long reviewer_id);
 }
