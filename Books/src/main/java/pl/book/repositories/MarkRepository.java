ccbc5e371a345153a846b7e4b973df9d2810b4aa
Wojciech Jurek <wojciech.jurek7@gmail.com>
2021-01-21 19:21:49 +0100

    Revert "Dodanie metod szukających oraz widoków dla Marks oraz Reviewers."
    This reverts commit bef7d439b94e7a5aa4d5befcfea788443e1ec89c.

--- Books/src/main/java/pl/book/repositories/MarkRepository.java
+++ Books/src/main/java/pl/book/repositories/MarkRepository.java
@@ -1,12 +1,9 @@
 package pl.book.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import pl.book.entities.Mark;
 
 public interface MarkRepository extends CrudRepository<Mark, Long> {
-	
-	@Query("SELECT m FROM Mark m WHERE m.book.book_id = ?1")
-	Iterable<Mark> findAllWhereBookId(Long book_id);
+
 }
