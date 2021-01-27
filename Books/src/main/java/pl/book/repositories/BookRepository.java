df1325a7b93ea07266cea6baf097c2c23e52db60
Wojciech Jurek <wojciech.jurek7@gmail.com>
2021-01-27 14:04:54 +0100

    Pass book when inserting mark.

--- Books/src/main/java/pl/book/repositories/BookRepository.java
+++ Books/src/main/java/pl/book/repositories/BookRepository.java
@@ -1,9 +1,13 @@
 package pl.book.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import pl.book.entities.Book;
+import pl.book.entities.Mark;
 
 public interface BookRepository extends CrudRepository<Book, Long>{
 
+	@Query("SELECT b FROM Book b WHERE b.book_id = ?1")
+	Iterable<Book> findAllWhereId(Long book_id);
 }
