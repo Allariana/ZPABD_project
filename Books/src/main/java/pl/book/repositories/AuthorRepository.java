74f7d3ffe7d72aeb90932c1b5fe08703f3a03862
IzabelaPieczek <233994@edu.p.lodz.pl>
2021-01-28 23:37:58 +0100

    Add new author

--- Books/src/main/java/pl/book/repositories/AuthorRepository.java
+++ Books/src/main/java/pl/book/repositories/AuthorRepository.java
@@ -1,9 +1,11 @@
 package pl.book.repositories;
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import pl.book.entities.Author;
 
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+	@Query("SELECT a FROM Author a WHERE a.id = ?1")
+	Iterable<Author> findAllWhereAuthorId(Long id);
 
 }
