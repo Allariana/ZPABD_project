02d33f1a54e35b4adf848b1c6a3467ad3249463f
Allariana <sochacka.kinga@gmail.com>
2021-01-29 10:14:44 +0100

    Add button for adding type if user is admin

--- Books/src/main/java/pl/book/controllers/TypeController.java
+++ Books/src/main/java/pl/book/controllers/TypeController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import pl.book.base.BaseController;
 import pl.book.entities.Book;
 import pl.book.entities.Type;
 import pl.book.manager.BookManager;
 import pl.book.manager.TypeManager;
 
 @Controller
-public class TypeController {
+public class TypeController extends BaseController {
 	@Autowired
 	private TypeManager typeManager;
 	
@@ -35,6 +36,7 @@ public class TypeController {
       Iterable<Type> types = typeManager.findAll();
       ModelAndView model = new ModelAndView("/allTypes.html");
       model.addObject("types", types);
+      model.addObject("isAdmin", String.valueOf(checkIfAuthenticatedUserIsAdmin()));
       return model;
   }
 }
